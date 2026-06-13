1f038643d42082c8b6262698beb28c89064ee6a3
Manuel <ernesto.ccastilloguerra2@gmail.com>
2026-06-13 17:19:24 -0600

    Falta ejecutar

---
+++ P7 Analisis02/Pagina2.xaml.cs
@@ -0,0 +1,17 @@
+namespace P7_Analisis02
+{
+    public partial class Pagina2 : ContentPage
+    {
+        
+
+        public Pagina2()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object? sender, EventArgs e)
+        {
+            
+        }
+    }
+}
